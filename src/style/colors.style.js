9571404f0731d08deb5d7fef083157deac3dc79f
Brian Fiszman <fiszmanbrian@gmail.com>
2023-09-09 14:34:00 -0300

    chore(style): Delete unnecessary file

--- src/style/colors.style.js
+++
@@ -1,6 +0,0 @@
-export const PRIMARY_COLOR = "#282c34";
-export const SECONDARY_COLOR = "#343028";
-
-export const TITLE_COLOR = "#4caf50";
-export const SUBTITLE_COLOR = "#686b70";
-export const LINK_COLOR = "#d95b43";
