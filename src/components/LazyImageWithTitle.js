137d408d0ded9da39d54e491b020f05d22890d96
Brian Fiszman <fiszmanbrian@gmail.com>
2024-08-13 20:17:01 -0300

    feat(zoom): add picture zoom on click functionality #4

--- src/components/LazyImageWithTitle.js
+++ src/components/LazyImageWithTitle.js
@@ -3,15 +3,15 @@ import { BarLoader } from "react-spinners";
 import { Picture } from "./Picture";
 import { Subtitle } from "./Subtitle";
 import Zoom from "react-medium-image-zoom";
+import 'react-medium-image-zoom/dist/styles.css'
 
 export function LazyImageWithTitle({ src, alt, size, children, theme }) {
   return (
     <Suspense fallback={<BarLoader color={theme.colors.primary} />}>
       <Subtitle theme={theme}>{children}</Subtitle>
       <Zoom>
-      <Picture src={src} alt={alt} size={size} loading="lazy" />
+        <Picture src={src} alt={alt} size={size} loading="lazy" />
       </Zoom>
     </Suspense>
   );
 }
-
