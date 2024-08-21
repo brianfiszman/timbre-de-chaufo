53968b0fc917510432fd7a4b9321244114fa0042
FraanBat <friggio.87@gmail.com>
2024-08-21 20:41:35 -0300

    feat(convert) Convert images format from jpeg and png to webp #7

--- src/routes/chaufest/Chaufest.js
+++ src/routes/chaufest/Chaufest.js
@@ -1,6 +1,6 @@
 import { MainHeader, Picture } from "../../components";
 import { theme } from "../../style/theme.style";
-import chaufest from "../../resources/img/chaufest.jpeg";
+import chaufest from "../../resources/img/chaufest.webp";
 
 function Chaufest() {
   return (
